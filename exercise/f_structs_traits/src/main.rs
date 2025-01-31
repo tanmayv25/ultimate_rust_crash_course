4fc461957f3e657da12b4392a967453eea28012f
Tanmay Verma <tanmayv@nvidia.com>
2025-01-31 10:10:09 +0000

    Exercise F: Structs and Traits

--- exercise/f_structs_traits/src/main.rs
+++ exercise/f_structs_traits/src/main.rs
@@ -5,34 +5,53 @@
 // `cargo run` without any errors.
 //
 //  trait Bite...
-
+trait Bite {
+    fn bite(self: &mut Self) {
+        println!("Default bite!!!");
+    }
+}
 
 // 2. Now create a struct named Grapes with a field that tracks how many grapes are left.  If you
 // need a hint, look at how it was done for Carrot at the bottom of this file (you should probably
 // use a different field, though).
 //
-// #[derive(Debug)] // include this line right before your struct definition
-// struct Grapes...
-
+#[derive(Debug)] // include this line right before your struct definition
+struct Grapes {
+    remaining_count: i32,
+}
 
 // 3. Implement Bite for Grapes.  When you bite a Grapes, subtract 1 from how many grapes are left.
 // If you need a hint, look at how it was done for Carrot at the bottom of this file.
 //
-// impl Bite for...
+impl Bite for Grapes {
+    fn bite(self: &mut Self) {
+        self.remaining_count -= 1;
+    }
+}
 
+fn bunny_nibbles<T: Bite>(food: &mut T) {
+    let count = 10;
+    for _i in 0..count {
+        food.bite();
+    }
+}
 
 fn main() {
     // Once you finish #1 above, this part should work.
-    let mut carrot = Carrot { percent_left: 100.0 };
+    let mut carrot = Carrot {
+        percent_left: 100.0,
+    };
     carrot.bite();
     println!("I take a bite: {:?}", carrot);
 
     // 4. Uncomment and adjust the code below to match how you defined your
     // Grapes struct.
     //
-    //let mut grapes = Grapes { amount_left: 100 };
-    //grapes.bite();
-    //println!("Eat a grape: {:?}", grapes);
+    let mut grapes = Grapes {
+        remaining_count: 100,
+    };
+    grapes.bite();
+    println!("Eat a grape: {:?}", grapes);
 
     // Challenge: Uncomment the code below. Create a generic `bunny_nibbles`
     // function that:
@@ -41,8 +60,8 @@ fn main() {
     // Hint: Define the generic type between the function name and open paren:
     //       fn function_name<T: Bite>(...)
     //
-    //bunny_nibbles(&mut carrot);
-    //println!("Bunny nibbles for awhile: {:?}", carrot);
+    bunny_nibbles(&mut carrot);
+    println!("Bunny nibbles for awhile: {:?}", carrot);
 }
 
 #[derive(Debug)] // This enables using the debugging format string "{:?}"
